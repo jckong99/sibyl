ba06f2ff64d2c3c7e57501866674f80977135dff
Nathan Hayes <nzhfold@comcast.net>
2019-04-13 15:00:31 -0400

    Thesaurus first test

--- src/sibyl/Thesaurus.java
+++ src/sibyl/Thesaurus.java
@@ -12,11 +12,24 @@ public class Thesaurus extends Retriever
 	
 	public Thesaurus(String word)
 	{
-		
+		try
+		{
+			doc = Jsoup.connect("https://www.thesaurus.com/browse/" + word.toLowerCase()).get();
+		}
+		catch(IOException e)
+		{
+			System.out.println("No available thesaurus entry for \"" + word + "\".");
+		}
 	}
 	
 	public String get()
 	{
-		
+		Elements e = doc.select(".css-1lc0dpe .et6tpn80");
+		String ret = "";
+		for(Element i : e)
+		{
+			ret += i.text();
+		}
+		return ret;
 	}
 }
