1eb57b8b871b34bcba15d4e9bd169f515329ee3a
Nathan Hayes <nzhfold@comcast.net>
2019-04-13 18:13:43 -0400

    .java files

--- src/sibyl/FileReader.java
+++ src/sibyl/FileReader.java
@@ -1,6 +1,54 @@
 package sibyl;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileReader 
 {
-
+	private String in_filename, out_filename;
+	private Retriever r;
+	public FileReader(String in_filename, String out_filename, Retriever r)
+	{
+		this.in_filename = in_filename;
+		this.out_filename = out_filename;
+		this.r = r;
+	}
+	public void process()
+	{
+		String filetext = "";
+		File file = new File(in_filename); 
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		while (sc.hasNextLine()) 
+		{
+			      filetext += sc.nextLine()+"\n";
+		}
+		int index = 0;
+		
+		while(filetext.indexOf(r.getWord(), index) != -1)
+		{
+			int wordloc = filetext.indexOf(r.getWord(), index);
+			filetext = filetext.substring(0, wordloc + r.getWord().length() - 1) + "("+r.get()+")"+filetext.substring(wordloc + r.getWord().length() - 1);
+			index = wordloc + 1;
+		}
+		
+	    try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(out_filename));
+			writer.write(filetext);
+		    writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     
+	}
 }
--- src/sibyl/Retriever.java
+++ src/sibyl/Retriever.java
@@ -6,6 +6,11 @@ public abstract class Retriever
 {
 	protected Document doc;
 	protected boolean valid;
+	protected String word;
 	
 	public abstract String get();
+	public String getWord()
+	{
+		return word;
+	}
 }
